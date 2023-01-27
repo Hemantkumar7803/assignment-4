b50270e2f96963954647a1a53b45eeef766193c4
Hemantkumar7803 <121182169+Hemantkumar7803@users.noreply.github.com>
2023-01-27 22:17:21 +0530

    Create Assignment 4 Q2.py

---
+++ Assignment 4 Q2.py
@@ -0,0 +1,12 @@
+# assignment-4
+a=int(input("enter your years"))
+if a%400==0:
+    print("it is  leap year")
+elif a%100==0:
+    print('it is not a leap year')
+elif a%4==0 :
+    print(" it is  a leap year")
+else :
+     print("it is not a leap year")
+
+
